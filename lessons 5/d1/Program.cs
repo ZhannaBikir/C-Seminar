b3de3780d0d68d5ec5c16be028884ae00782e51c
ZhannaBikir <Zhannabikir@mail.ru>
2022-12-27 22:07:49 +0300

    Урок 5 Домашка 1

---
+++ lessons 5/d1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = new int [a];
+Random rnd = new Random();
+
+Console.WriteLine("Вывод массива: ");
+Console.Write("[ ");
+
+for (int i=0; i<myArray.Length; i++)
+{
+    myArray[i]=rnd.Next(100, 999);
+}
+
+for (int i=0; i<myArray.Length; i++)
+{
+    Console.Write(" " + myArray[i] + " ");
+}
+Console.WriteLine(" ]");
+
+int count = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] % 2 == 0)
+    {
+        count = count + 1;
+    }
+
+}
+Console.WriteLine("Количетво четных чисел = " + count);
