2f173a4b9c913f0f1dbcd413e930889cb27b21c8
ZhannaBikir <Zhannabikir@mail.ru>
2022-12-27 20:28:49 +0300

    Урок 4 задача 7

--- lessons 4/7/Program.cs
+++ lessons 4/7/Program.cs
@@ -1,28 +1,49 @@
 ﻿// Дан массив размера N. Найти максимальный из его локальных минимумов. 
 
-Console.Write("Введите количество элементов массива: ");
-
-int a = Convert.ToInt32(Console.ReadLine());
-int[] myArray = new int[a]; 
-Random rnd = new Random ();
-
-Console.WriteLine("Вывод массива: ");
-Console.Write("[ ");
-
-for (int i = 0; i < myArray.Length; i++)
+internal class Program
 {
-    myArray[i] = rnd.Next(1, 100);
-    Console.Write(" " + myArray[i] + " "); 
-}
-
-Console.WriteLine(" ]");
-
-int minIndex = 0;
-for (int i = 0; i<myArray.Length; i++)
-{
-    if (myArray[minIndex]>myArray[i])
+    private static void Main(string[] args)
     {
-        minIndex = i;   
+        Console.Write("Введите количество элементов массива: ");
+
+        int a = Convert.ToInt32(Console.ReadLine());
+        int[] myArray = new int[a];
+        Random rnd = new Random();
+        int[] myArray2 = new int[a];
+
+        Console.WriteLine("Вывод массива: ");
+        Console.Write("[ ");
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            myArray[i] = rnd.Next(1, 100);
+            Console.Write(" " + myArray[i] + " ");
+        }
+
+        Console.WriteLine(" ]");
+
+        for (int i = 1; i < myArray.Length - 1; i++)
+        {
+            int left = myArray[i - 1];
+            int middle = myArray[i];
+            int right = myArray[i + 1];
+
+            if (left > middle && right > middle)
+            {
+                myArray2[i] = middle;
+               // Console.WriteLine(" " + myArray2[i] + " ");
+            }
+        }
+        
+        int max = 0;
+        for (int i = 0; i < myArray2.Length; i++)
+        {
+            if (myArray2[i] > max)
+            {
+                max = myArray2[i];
+            }
+        }
+
+        Console.Write("Максимальное из локальных минимумов: " + max);
     }
 }
-Console.WriteLine("Локальные минимумы массива: " + minIndex);
