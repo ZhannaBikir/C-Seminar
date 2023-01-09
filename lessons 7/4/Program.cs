5041a1021056b41be13a22209f12aa9ea18d9a00
ZhannaBikir <Zhannabikir@mail.ru>
2023-01-09 22:52:28 +0300

    7 урок

---
+++ lessons 7/4/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumInArray(int[,] inArray)
+{
+    int sum = 0;
+    int length = inArray.GetLength(0) > inArray.GetLength(1) ? inArray.GetLength(1) : inArray.GetLength(0);
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += inArray[i, i];
+    }
+
+    return sum;
+
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов массива: ");
+int cols = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+int summ = SumInArray(array);
+Console.Write("Сумма элементов главной диагонали: " + summ);
+
---
+++ lessons 7/d2/Program.cs
@@ -0,0 +1,64 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+Console.Write("Введите кол-во строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов массива: ");
+int cols = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число, которое необходимо найти: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+CountElements(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CountElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == a)
+            {
+                Console.WriteLine(a);
+                break;
+            }
+        }
+
+    }
+    Console.WriteLine("Введенного числа нет в массиве");
+}
+
+
