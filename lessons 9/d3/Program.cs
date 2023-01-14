0ced00d34d172e2c6148f0abe4de1d2bd0b5e342
ZhannaBikir <Zhannabikir@mail.ru>
2023-01-14 23:56:32 +0300

    Жопа

--- lessons 9/d3/Program.cs
+++ lessons 9/d3/Program.cs
@@ -4,20 +4,20 @@
 // m = 3, n = 2 -> A(m,n) = 29
 Console.Write("Введите первое число: ");
 int n = int.Parse(Console.ReadLine()!);
-Console.Write("Введте второе число(обязательно больше первого): ");
+Console.Write("Введте второе число: ");
 int m = int.Parse(Console.ReadLine()!);
 
 int AkkermanFunc(int m, int n)
-		{
-			if(m == 0)
-			{
-				return n + 1;
-			}
-			if(m > 0 && n == 0)
-			{
-				return AkkermanFunc(m - 1, 1);
-			}
-			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
-		}
-		
-		Console.WriteLine(AkkermanFunc(n,m));
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AkkermanFunc(m - 1, 1);
+    }
+    return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+}
+
+Console.WriteLine(AkkermanFunc(n, m));
